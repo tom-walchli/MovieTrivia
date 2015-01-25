a34240882782c6b00ec92e19c6229ad63cf4e28e
Tom Walchli <tom@wit-americas.com>
2015-01-25 18:08:19 -0500

    cleanup

--- lib/movie.rb
+++ lib/movie.rb
@@ -1,47 +1,6 @@
 require 'imdb'
 require 'pry'
 
-
-
-# class Movie_old
-#     attr_accessor :id, :title, :poster, :year
-#     def initialize(options={})
-#     	@id = options[:id] if options[:id]
-#     	@title = options[:title] if options[:title]
-#     	@poster = options[:poster] if options[:poster]
-#     	@year = options[:year] if options[:year]
-#     end
-
-#     def pretty_title
-#     	self.title.capitalize
-#     end
-
-# 	def search_old(term)
-# 	  results = []
-#       imdb_results = Imdb::Search.new(term).movies
-#       binding.pry
-      
-#       results = imdb_results.map{|m|
-#         Movie.new(id: m.id, title: m.title,poster: m.poster,year: m.year)
-#       }
-#       binding.pry
-#       results = results.map{|m| m if !m.poster.nil?}.compact
-#       results
-# 	end
-# 	def search(term)
-# 	  results = []
-#       imdb_results = Imdb::Search.new(term).movies.map{|m| m if !m.poster.nil?}.compact.sample(9)
-#       binding.pry
-      
-#       results = imdb_results.map{|m|
-#         Movie.new(id: m.id, title: m.title,poster: m.poster,year: m.year)
-#       }
-#       binding.pry
-#       results = results
-#       results
-# 	end
-# end
-
 class Movie
 	attr_reader :movies
 	def initialize(pattern)
@@ -53,9 +12,11 @@ class Movie
  		movs = Imdb::Search.new(pattern)
  		puts "#{movs.movies.length} movies found matching pattern '#{pattern}'"
 
-
  		movies = []
  		movies_groups = []
+
+ 		# taking samples from earlier slices increases the chance of finding posters and 
+ 		# returns movies better related to the search term
  		movs.movies.each_slice(slice_each) {|group| movies_groups.push(group)}
  		movies_groups.each do |group|
  			puts "switching group..."
